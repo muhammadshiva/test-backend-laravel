279ea1eb2a4781149d9fe9d15ecc7f925379f67c
muhammadshiva <shivamuhammad07@gmail.com>
2022-11-08 08:29:05 +0700

    (model) : create Money Plan Category

---
+++ app/Models/MoneyPlanCategory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MoneyPlanCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'thumbnail',
+    ];
+
+    public function moneyPlans()
+    {
+        return $this->hasMany(MoneyPlan::class, 'category_id', 'id');
+    }
+}
