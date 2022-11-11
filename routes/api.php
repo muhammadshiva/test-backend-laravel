2a2d32aa2dfce5f6bed24b658befcfb84da940ad
muhammadshiva <shivamuhammad07@gmail.com>
2022-11-11 09:07:07 +0700

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\PaymentMethodController;
 use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\TipController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\WalletController;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,9 +27,15 @@ Route::get('products', [ProductController::class, 'all']);
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
+Route::get('tips', [TipController::class, 'fetch']);
+Route::get('payment_methods', [PaymentMethodController::class, 'all']);
+
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [UserController::class, 'fetch']);
     Route::post('user', [UserController::class, 'updateProfile']);
     Route::post('logout', [UserController::class, 'logout']);
     Route::post('is-email-exist', [UserController::class, 'isEmailExist']);
+    Route::get('wallets', [WalletController::class, 'fetch']);
+    Route::post('wallets', [WalletController::class, 'updatePin']);
 });
