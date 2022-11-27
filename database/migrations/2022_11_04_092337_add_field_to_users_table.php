51f2b639eba92287fc899f9839dd93ad39c4ad86
muhammadshiva <shivamuhammad07@gmail.com>
2022-11-27 18:26:27 +0700

    change nullable to username & ktp

--- database/migrations/2022_11_04_092337_add_field_to_users_table.php
+++ database/migrations/2022_11_04_092337_add_field_to_users_table.php
@@ -14,9 +14,9 @@ class AddFieldToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->after('email')->unique();
+            $table->string('username')->after('email')->nullable();
             $table->boolean('verified')->after('email')->nullable();
-            $table->string('ktp')->after('email');
+            $table->string('ktp')->after('email')->nullable();
         });
     }
 
