8d46d646bb98230a6549e6ef8bc7a1d7c3644e4c
muhammadshiva <shivamuhammad07@gmail.com>
2022-11-27 18:23:01 +0700

    fixing fetch

--- app/Http/Controllers/API/MoneyPlanController.php
+++ app/Http/Controllers/API/MoneyPlanController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseCustom;
 use App\Http\Controllers\Controller;
 use App\Models\MoneyPlan;
 use Illuminate\Http\Request;
-use App\Helpers\ResponseFormatter;
 use App\Models\MoneyPlanCategory;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Wallet;
@@ -14,12 +14,14 @@ class MoneyPlanController extends Controller
 {
     public function fetch(Request $request)
     {
+        // $moneyPlan = MoneyPlan::with('moneyPlansCategories')->where('category_id', $request->id)->paginate($request->limit);
 
-        $plan = MoneyPlan::paginate($request->limit);
+        $user = Auth::user();
+        $plan = MoneyPlanCategory::with('moneyPlans')->where('user_id', $user->id)->paginate($request->limit);
 
-        return ResponseFormatter::success([
+        return ResponseCustom::success(
             $plan,
-        ]);
+        );
     }
 
     public function create(Request $request)
@@ -44,7 +46,7 @@ class MoneyPlanController extends Controller
             'balance' => $checkPin->balance - $amount,
         ]);
 
-        return ResponseFormatter::success([
+        return ResponseCustom::success([
             'message' => $moneyPlan,
         ]);
     }
